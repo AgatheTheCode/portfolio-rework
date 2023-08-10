0b658236866445d746ec8a166aaa695087c109f3
Agathe <agathe.dufour.marin@gmail.com>
2023-08-10 20:52:00 +0200

    gitflow-hotfix-stash: ChartJS

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,41 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <DoughnutChart :chartData="testData" />
+  <BarChart :chartData="testData" />
+  <LineChart :chartData="testData" />
+  <BarChart :chartData="testData" />
+  <PieChart :chartData="testData" />
 </template>
 
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
+<script setup lang="ts">
+import { defineComponent } from 'vue'
+import { LineChart, DoughnutChart, BarChart, BubbleChart, PieChart } from 'vue-chart-3'
+import { Chart, registerables } from 'chart.js'
+import { ref } from 'vue'
+
+Chart.register(...registerables)
+///LETIABLES
+let labels = []
+let labelName: string
+let data = []
+let datum: number
+let colors = []
+let colorSingle: string
+
+function MyChart() {
+  labels = ['Paris', 'Nîmes', 'Toulon', 'Perpignan', 'Autre']
+  data = [30, 40, 60, 70, 5]
+  colors = ['#77CEFF', '#63ab51', '#123E6B', '#97B0C4', '#A5C8ED']
 }
-</style>
+
+MyChart()
+///REF
+let testData = ref({
+  labels: labels,
+  datasets: [
+    {
+      data: data,
+      backgroundColor: colors
+    }
+  ]
+})
+</script>
