f728e65926b06d2d7a5ed0e0c23b8372bf269ce3
Agathe <agathe.dufour.marin@gmail.com>
2024-06-18 14:59:37 +0200

    gitflow-hotfix-stash: fixLienImageFooter

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -7,15 +7,15 @@ import { defineComponent } from 'vue'
     <div class="links">
       <h3>Links</h3>
       <div class="links_div">
-        <a href="https://www.linkedin.com/in/agathe-dufour" target="_blank"
-          ><img src="assets/images/lkdn.png" alt=""
-        /></a>
-        <a href="https://github.com/AgatheTheCode/" target="_blank"
-          ><img src="assets/images/gthb.png" alt=""
-        /></a>
-        <a href="/assets/files/resume.pdf" target="_blank"
-          ><img src="assets/images/flag.png" alt=""
-        /></a>
+        <a href="https://www.linkedin.com/in/agathe-dufour" target="_blank">
+          <img src="/src/assets/images/lkdn.png" alt="" />
+        </a>
+        <a href="https://github.com/AgatheTheCode/" target="_blank">
+          <img src="/src/assets/images/gthb.png" alt="" />
+        </a>
+       <!-- <a href="/src/assets/files/resume.pdf" target="_blank"
+          ><img src="/src/assets/images/flag.png" alt=""
+        /></a> -->
       </div>
     </div>
     <div class="credit">
